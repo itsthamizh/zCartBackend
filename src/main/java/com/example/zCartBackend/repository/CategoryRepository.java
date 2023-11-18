31e224fbbf011105bf13032f60a1fde666a8d7a2
itsthamizh <muthamizhselvan0402@gmail.com>
2023-11-18 14:38:39 +0530

    adding category from custom db file

--- src/main/java/com/example/zCartBackend/repository/CategoryRepository.java
+++ src/main/java/com/example/zCartBackend/repository/CategoryRepository.java
@@ -1,10 +1,17 @@
 package com.example.zCartBackend.repository;
 
 import com.example.zCartBackend.model.Category;
+import com.example.zCartBackend.model.Inventories;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, String> {
+    @Query("SELECT i FROM Category i WHERE i.categoryName = :categoryName")
+    Optional<Category> findByCategory(@Param("categoryName") String categoryName);
 
 }
